616319d944db6b4824c6ea1d04de9908904b5927
mohsin ali <mohsinalisurhio08@gmail.com>
2024-12-30 00:09:28 +0500

    update changes

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,13 +1,12 @@
 const conf = {
 	apiKey: String(import.meta.env.VITE_FIREBASE_API_KEY),
-	authDomain: String(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN),
+	authDomain: String(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN), // Correct key here
 	projectId: String(import.meta.env.VITE_FIREBASE_PROJECT_ID),
 	storageBucket: String(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET),
-	messagingSenderId: String(
-		import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID
-	),
+	messagingSenderId: String(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
 	appId: String(import.meta.env.VITE_FIREBASE_APP_ID),
 	measurementId: String(import.meta.env.VITE_FIREBASE_MEASUREMENT_ID),
-};
-
-export default conf;
+  };
+  
+  export default conf;
+  
